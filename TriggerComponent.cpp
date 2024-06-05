bc631292596dab5323df9162f747f052b6c31fe0
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2024-06-05 06:18:21 -0400

    Add files via upload

--- TriggerComponent.cpp
+++ TriggerComponent.cpp
@@ -3,9 +3,22 @@
 
 #include "TriggerComponent.h"
 
+UTriggerComponent::UTriggerComponent()
+{
+    PrimaryComponentTick.bCanEverTick = true;
+
+
+}
+
 void UTriggerComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-    UE_LOG(LogTemp, Display, TEXT("Trigger Compionent alive"));
+}
+
+void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+
 }
